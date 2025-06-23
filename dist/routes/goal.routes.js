e604db1be9b787021f266581e7e98775f78f5333
DiyorbekTursunov <diyorbektursunov570@gmail.com>
2025-06-23 18:22:28 +0500

    fixed error

---
+++ dist/routes/goal.routes.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const auth_middleware_1 = require("../middleware/auth.middleware");
+const goalController_1 = require("../controllers/goalController");
+const router = (0, express_1.Router)();
+// CRUD routes for goals (all protected by authentication)
+router.post("/goals", auth_middleware_1.authenticate, (req, res, next) => {
+    (0, goalController_1.createGoal)(req, res).catch(next);
+});
+router.get("/goals", auth_middleware_1.authenticate, (req, res, next) => {
+    (0, goalController_1.getGoals)(req, res).catch(next);
+});
+router.get("/goals/public", auth_middleware_1.authenticate, (req, res, next) => {
+    (0, goalController_1.getPublicGoals)(req, res).catch(next);
+});
+router.get("/goals/:id", auth_middleware_1.authenticate, (req, res, next) => {
+    (0, goalController_1.getGoalById)(req, res).catch(next);
+});
+router.put("/goals/:id", auth_middleware_1.authenticate, (req, res, next) => {
+    (0, goalController_1.updateGoal)(req, res).catch(next);
+});
+router.delete("/goals/:id", auth_middleware_1.authenticate, (req, res, next) => {
+    (0, goalController_1.deleteGoal)(req, res).catch(next);
+});
+router.post("/goals/:id/join", auth_middleware_1.authenticate, (req, res, next) => {
+    (0, goalController_1.joinGoal)(req, res).catch(next);
+});
+router.get("/goals/joined", auth_middleware_1.authenticate, (req, res, next) => {
+    (0, goalController_1.getJoinedGoals)(req, res).catch(next);
+});
+router.get("/goals/search", (req, res, next) => {
+    (0, goalController_1.searchPublicGoals)(req, res).catch(next);
+});
+exports.default = router;
