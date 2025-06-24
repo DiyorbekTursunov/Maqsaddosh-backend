47a9423d43da41c3b6520389d0fea873202a0dc1
DiyorbekTursunov <diyorbektursunov570@gmail.com>
2025-06-24 20:36:12 +0500

    last of us

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,16 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
-import directionRoutes from './routes/direction.routes';
-import goalRoutes from './routes/goal.routes';
-import userRoutes from './routes/user.route';
-import cors from 'cors';
+import express, { Application, Request, Response, NextFunction } from "express";
+import directionRoutes from "./routes/direction.routes";
+import goalRoutes from "./routes/goal.routes";
+import userRoutes from "./routes/user.route";
+import cors from "cors";
 
 const app: Application = express();
 
-// SIMPLEST CORS FIX - Put this BEFORE everything else
-app.use(
-  cors({
-    origin: [
-      "https://maqsaddosh-backend-o2af.onrender.com",
-      "http://localhost:3000",
-    ], // Allow Swagger UI and local dev
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors()); // Allows all origins
 
 app.use(express.json());
-app.use('/api', directionRoutes);
-app.use('/api', goalRoutes);
-app.use('/api', userRoutes);
+app.use("/api", directionRoutes);
+app.use("/api", goalRoutes);
+app.use("/api", userRoutes);
 
 export default app;
